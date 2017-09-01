55b3bf9e00cc0f2f91d437fa5c4bc93bea97a594
Dan Cocking <Dan.Cocking@gmail.com>
2017-08-31 19:02:21 -0500

    better work harder

--- MWDTCdata/settings.py
+++ MWDTCdata/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'MWDTCdata.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DanCocking.mysql.pythonanywhere-services.com',
+        'USER': 'DanCocking',
+        'PASSWORD': 'PHosfavlapafBo5',
+        'HOST': 'DanCocking.mysql.pythonanywhere-services.com',
     }
 }
 
